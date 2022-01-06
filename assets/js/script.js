65b37503bc79346d0ac97e2058a329e2d1e581e1
Nate <natesvoboda64@gmail.com>
2022-01-06 17:28:51 -0500

    adding initial JS variables

---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+var citiesListArr = [];
+var numOfCities = 9;
+var personalAPIKey = "appid=350d6a65f722e2f68993f8e83b2513f1";
+var unit = "units=imperial";
+var dailyWeatherApiStarts = "https://api.openweathermap.org/data/2.5/weather?q=";
+var dailyUVIndexApiStarts = "https://api.openweathermap.org/data/2.5/uvi?";
+var forecastWeatherApiStarts = "https://api.openweathermap.org/data/2.5/onecall?";
